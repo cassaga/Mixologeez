8e120cfa8541bf1173c1d6a661412b42f59c9131
cassaga <cassagnea@gmail.com>
2023-09-12 15:29:37 +0200

    tagcontroller

--- src/main/java/com/mixologeez/demo/controller/TagController.java
+++ src/main/java/com/mixologeez/demo/controller/TagController.java
@@ -1,6 +1,6 @@
 package com.mixologeez.demo.controller;
 
-i
+
 import com.mixologeez.demo.models.Tag;
 import com.mixologeez.demo.repositories.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,31 +8,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @RestController
+@RequestMapping("/v1/tag")
 public class TagController {
 
     private final TagRepository tagRepository;
 
     @Autowired
     public TagController(TagRepository tagRepository) {
-        this.tagRepository = tagRepository;
+        this.tagRepository =tagRepository;
     }
 
-    @GetMapping("/tag/v1")
+    @GetMapping
     public List<Tag> getAllTag() {
         return (List<Tag>) tagRepository.findAll();
     }
 
-    @GetMapping("/tag/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Tag> getTagById(@PathVariable Integer id) {
-        Optional<Tag> tag = tagRepository.findById(Math.toIntExact(id));
-        return tag.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        return tagRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/tag")
-    public Tag addTag(@RequestBody Tag tag ){
+    @PostMapping
+    public Tag addYTag(@RequestBody Tag tag) {
         return tagRepository.save(tag);
     }
 }
