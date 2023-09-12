7c7fa3e4a804b29ffad542ce34d100b693614b3c
Julien <julient.bourgeois@gmail.com>
2023-09-12 15:00:45 +0200

    Correction des services Cocktail et Tag

--- src/main/java/com/mixologeez/demo/services/CocktailService.java
+++ src/main/java/com/mixologeez/demo/services/CocktailService.java
@@ -1,8 +1,37 @@
 package com.mixologeez.demo.services;
 
+import com.mixologeez.demo.models.Cocktail;
+import com.mixologeez.demo.repositories.CocktailRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CocktailService {
 
+    private final CocktailRepository cocktailRepository;
+
+    @Autowired
+    public CocktailService(CocktailRepository cocktailRepository) {
+        this.cocktailRepository = cocktailRepository;
+    }
+
+    public List<Cocktail> findAll() {
+        return (List<Cocktail>) cocktailRepository.findAll();
+    }
+
+    public Optional<Cocktail> findById(Integer id) {
+        return cocktailRepository.findById(id);
+    }
+
+    public Cocktail save(Cocktail cocktail) {
+        return cocktailRepository.save(cocktail);
+    }
+
+    public void deleteById(Integer id) {
+        cocktailRepository.deleteById(id);
+    }
 }
--- src/main/java/com/mixologeez/demo/services/TagService.java
+++ src/main/java/com/mixologeez/demo/services/TagService.java
@@ -1,7 +1,36 @@
 package com.mixologeez.demo.services;
 
+import com.mixologeez.demo.models.Tag;
+import com.mixologeez.demo.repositories.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TagService {
+
+    private final TagRepository tagRepository;
+
+    @Autowired
+    public TagService(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
+
+    public List<Tag> findAll() {
+        return (List<Tag>) tagRepository.findAll();
+    }
+
+    public Optional<Tag> findById(Integer id) {
+        return tagRepository.findById(id);
+    }
+
+    public Tag save(Tag tag) {
+        return tagRepository.save(tag);
+    }
+
+    public void deleteById(Integer id) {
+        tagRepository.deleteById(id);
+    }
 }
