5a5c4d8d08d4c7e634fcfacbe204406a94b6b587
cassaga <cassagnea@gmail.com>
2023-09-12 15:49:39 +0200

    controllers tag & country

--- src/main/java/com/mixologeez/demo/controller/TagController.java
+++ src/main/java/com/mixologeez/demo/controller/TagController.java
@@ -2,7 +2,7 @@ package com.mixologeez.demo.controller;
 
 
 import com.mixologeez.demo.models.Tag;
-import com.mixologeez.demo.repositories.TagRepository;
+import com.mixologeez.demo.services.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,27 +14,26 @@ import java.util.List;
 @RequestMapping("/v1/tag")
 public class TagController {
 
-    private final TagRepository tagRepository;
+    private final TagService tagService;
 
     @Autowired
-    public TagController(TagRepository tagRepository) {
-        this.tagRepository =tagRepository;
-    }
+    public TagController(TagService tagService)  { this.tagService =tagService;}
+
 
     @GetMapping
     public List<Tag> getAllTag() {
-        return (List<Tag>) tagRepository.findAll();
+        return (List<Tag>) tagService.findAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Tag> getTagById(@PathVariable Integer id) {
-        return tagRepository.findById(id)
+        return tagService.findById(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
     public Tag addYTag(@RequestBody Tag tag) {
-        return tagRepository.save(tag);
+        return tagService.save(tag);
     }
 }
