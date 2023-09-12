aab5e3967808d6825ba594b8cf36fa3a8ad8e457
cassaga <cassagnea@gmail.com>
2023-09-12 15:25:55 +0200

    Auto stash before merge of "main" and "origin/main"

--- src/main/java/com/mixologeez/demo/controller/TagController.java
+++ src/main/java/com/mixologeez/demo/controller/TagController.java
@@ -1,4 +1,38 @@
 package com.mixologeez.demo.controller;
 
+i
+import com.mixologeez.demo.models.Tag;
+import com.mixologeez.demo.repositories.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class TagController {
+
+    private final TagRepository tagRepository;
+
+    @Autowired
+    public TagController(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
+
+    @GetMapping("/tag/v1")
+    public List<Tag> getAllTag() {
+        return (List<Tag>) tagRepository.findAll();
+    }
+
+    @GetMapping("/tag/{id}")
+    public ResponseEntity<Tag> getTagById(@PathVariable Integer id) {
+        Optional<Tag> tag = tagRepository.findById(Math.toIntExact(id));
+        return tag.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/tag")
+    public Tag addTag(@RequestBody Tag tag ){
+        return tagRepository.save(tag);
+    }
 }
