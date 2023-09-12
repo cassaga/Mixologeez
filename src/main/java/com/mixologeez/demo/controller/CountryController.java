8696f32ffb97e9a63565a948f2e05acfb67edd23
cassaga <cassagnea@gmail.com>
2023-09-12 15:02:51 +0200

    Auto stash before merge of "main" and "origin/main"

--- src/main/java/com/mixologeez/demo/controller/CountryController.java
+++ src/main/java/com/mixologeez/demo/controller/CountryController.java
@@ -1,4 +1,41 @@
 package com.mixologeez.demo.controller;
 
+import com.mixologeez.demo.models.Country;
+import com.mixologeez.demo.repositories.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class CountryController {
+
+    private final CountryRepository countryRepository;
+
+    @Autowired
+    public CountryController(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @GetMapping("/country/v1")
+    public List<Country> getAllCountries() {
+        return (List<Country>) countryRepository.findAll();
+    }
+
+    @GetMapping("/country/{id}")
+    public ResponseEntity<Country> getCountryById(@PathVariable Long id) {
+        Optional<Country> country = countryRepository.findById(Math.toIntExact(id));
+        if (country.isPresent()) {
+            return ResponseEntity.ok(country.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/country")
+    public Country addCountry(@RequestBody Country country) {
+        return countryRepository.save(country);
+    }
 }
